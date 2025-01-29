aadcfbd7242204ae2019998c40a01535764c9e98
Laine Smith <39713102+lainie-ftw@users.noreply.github.com>
2025-01-29 13:19:17 -0500

    Update __init__.py

--- custom_components/counterstrike/__init__.py
+++ custom_components/counterstrike/__init__.py
@@ -1,1 +1,28 @@
-"""Add logic later"""
+"""
+The "hello world" custom component.
+
+This component implements the bare minimum that a component should implement.
+
+Configuration:
+
+To use the hello_world component you will need to add the following to your
+configuration.yaml file.
+
+counterstrike:
+"""
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+# The domain of your component. Should be equal to the name of your component.
+DOMAIN = "counterstrike"
+
+
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up a skeleton component."""
+    # States are in the format DOMAIN.OBJECT_ID.
+    hass.states.set('counterstrike.Hello_World', 'Works!')
+
+    # Return boolean to indicate that initialization was successfully.
+    return True
